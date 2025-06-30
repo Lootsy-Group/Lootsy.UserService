319f89f97948e8300d77524e0bd3c9b6d843b688
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-06-30 23:58:38 +0500

    Add JwtTokenService

---
+++ Lootsy.UserService.Application/Services/JwtTokenService.cs
@@ -0,0 +1,41 @@
+﻿using Lootsy.UserService.Application.Configurations;
+using Lootsy.UserService.Application.Interfaces;
+using Lootsy.UserService.Domain.Aggregates;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Lootsy.UserService.Application.Services;
+
+internal sealed class JwtTokenService : IJwtTokenService
+{
+    private readonly JwtOptions _options;
+
+    public JwtTokenService(IOptions<JwtOptions> options)
+    {
+        _options = options.Value ?? throw new ArgumentNullException(nameof(options));
+    }
+
+    public string GenerateToken(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new(ClaimTypes.Name, user.UserName!)
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _options.Issuer,
+            audience: _options.Audience,
+            claims: claims,
+            expires: DateTime.UtcNow.AddHours(_options.ExpiresInHours),
+            signingCredentials: creds);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
