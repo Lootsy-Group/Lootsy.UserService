106021da13c6ee3d113caaa5153b8a3b7f8a8a83
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-06-30 23:56:16 +0500

    Add Email Service

---
+++ Lootsy.UserService.Application/Interfaces/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using Lootsy.UserService.Application.Models;
+
+namespace Lootsy.UserService.Application.Interfaces;
+
+public interface IEmailService
+{
+    void SendEmailConfirmation(EmailMessage emailMessage);
+    void SendPasswordReset(EmailMessage emailMessage);
+}
---
+++ Lootsy.UserService.Infrastructure/Email/EmailService.cs
@@ -0,0 +1,32 @@
+﻿using FluentEmail.Core;
+using Lootsy.UserService.Application.Interfaces;
+using Lootsy.UserService.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lootsy.UserService.Infrastructure.Email;
+
+internal sealed class EmailService : IEmailService
+{
+    private readonly IFluentEmail _fluentEmail;
+
+    public EmailService(IFluentEmail fluentEmail)
+    {
+        _fluentEmail = fluentEmail ?? throw new ArgumentNullException(nameof(fluentEmail));
+    }
+
+    public void SendEmailConfirmation(EmailMessage emailMessage)
+    {
+        _fluentEmail.To(emailMessage.To)
+            .Subject(emailMessage.Subject)
+            .Send();
+    }
+
+    public void SendPasswordReset(EmailMessage emailMessage)
+    {
+        throw new NotImplementedException();
+    }
+}
