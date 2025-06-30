d8cb6e847d8831ac20e72f7286224c85b734c097
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-07-01 00:00:45 +0500

    Implement SendEmail Code Command and Handler

--- Lootsy.UserService.Application/Features/Commands/SendEmailCodeCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿using Lootsy.UserService.Application.Extensions;
-using MediatR;
-
-namespace Lootsy.UserService.Application.Features.Commands;
-
-public record SendEmailCodeCommand(
-    string Email
-    ) : IRequest<Result>;
